2f937a85685e64d8079e36ff75f4abcaaf22cece
SRISHTI KUMARI <125398898+Srishtikumari510@users.noreply.github.com>
2026-04-24 14:12:21 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,1 +1,36 @@
-PASTE MODEL CODE HERE
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class PrunableLinear(nn.Module):
+    def __init__(self, in_features, out_features):
+        super().__init__()
+        
+        self.weight = nn.Parameter(torch.randn(out_features, in_features) * 0.01)
+        self.bias = nn.Parameter(torch.zeros(out_features))
+        
+        # Learnable gate scores
+        self.gate_scores = nn.Parameter(torch.randn(out_features, in_features))
+
+    def forward(self, x):
+        gates = torch.sigmoid(self.gate_scores)   # values between 0 and 1
+        pruned_weight = self.weight * gates       # apply gating
+        return F.linear(x, pruned_weight, self.bias)
+
+
+class PrunableNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+        self.fc1 = PrunableLinear(32*32*3, 512)
+        self.fc2 = PrunableLinear(512, 256)
+        self.fc3 = PrunableLinear(256, 10)
+
+    def forward(self, x):
+        x = x.view(x.size(0), -1)  # flatten image
+        
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        
+        return x
